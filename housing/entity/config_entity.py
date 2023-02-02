6663989d00006aaa6e769cbddfa32deb36039af4
Balaji Haridasan <balajiharidasan@gmail.com>
2023-02-02 17:45:17 +0530

    updated

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -1,10 +1,6 @@
 from collections import namedtuple
 
+DataIngestionConfig=namedtuple("DataIngestionConfig",  
+["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])  # information required for data ingestion
 
-DataIngestionConfig=namedtuple("DataIngestionConfig",
-["dataset_download_url","raw_data_dir","ingested_train_dir","ingested_test_dir"]) # information required for the data ingestion configuration
-
-
-
-DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path","report_file_path","report_page_file_path"])  # information required for the data validation config
 
