cbcbec5ec1fac60ff0b373bfdacd6c85ffc1f0c6
Balaji Haridasan <balajiharidasan@gmail.com>
2023-02-03 11:09:53 +0530

    updated

--- Car/logger/__init__.py
+++ Car/logger/__init__.py
@@ -18,3 +18,6 @@ LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
 
 logging.basicConfig(filename=LOG_FILE_PATH,filemode='w',format='[%(asctime)s]-%(name)s-%(levelname)s-%(message)s',level=logging.INFO)
+
+
+#################
